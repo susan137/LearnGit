830bbe458531520c65e39964163eab48b0898c21
susan137 <40704845+susan137@users.noreply.github.com>
2021-12-23 11:41:53 +1300

    Add files via upload
    Uploading my first file

---
+++ TestMaven/src/main/java/testproperties/PropertyFileReader.java
@@ -0,0 +1,16 @@
+package testproperties;
+
+import java.io.*;
+import java.util.Properties;
+
+public class PropertyFileReader {
+
+	public static void main(String[] args) throws IOException {
+		FileReader fr=new FileReader("//Users//susan//eclipse-workspace//TestMaven//src//main//resources//login.properties");
+		Properties pr=new Properties();
+		pr.load(fr);
+		System.out.println(pr.getProperty("userid"));
+		System.out.println(pr.getProperty("password"));
+	}
+
+}
